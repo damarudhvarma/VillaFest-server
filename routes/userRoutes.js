8c12e44b12c3119deae88dd10efaf0e8a03af51d
damarudhvarma <damarudhvarma.dandu@gmail.com>
2025-03-28 20:35:37 +0530

    get users endpoint ✨

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { loginUserController, registerUserController } from '../controllers/userController.js';
+import { getAllUsersController, loginUserController, registerUserController } from '../controllers/userController.js';
+import { verifyToken } from '../middlewares/authMiddleware.js';
 
 
 const userRouter = Router();
 
 userRouter.post('/register', registerUserController);
 userRouter.post('/login', loginUserController);
-
+userRouter.get('/get-all-users',verifyToken, getAllUsersController);
 
 
 
