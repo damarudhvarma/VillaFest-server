44963555aa73034d69a4d194dfb29a0a1e24d424
damarudhvarma <damarudhvarma.dandu@gmail.com>
2025-03-31 22:33:40 +0530

    CORS setup

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 // Configure CORS with specific origin
 app.use(cors({
-    origin: '*', // Allow requests from any origin
+    origin: [process.env.USER_PRODUCTION_URL,process.env.ADMINPANEL_URL,process.UserSide_URL], // Allow requests from any origin
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
