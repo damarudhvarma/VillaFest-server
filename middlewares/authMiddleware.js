64739b6b423ac607a421de54bfa941cb03c7fe70
damarudhvarma <damarudhvarma.dandu@gmail.com>
2025-04-25 22:33:35 +0530

    📝

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -72,12 +72,7 @@ export const authenticateHost = async (req, res, next) => {
     try {
         // Get token from header
         const authHeader = req.headers.authorization;
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return res.status(401).json({
-                success: false,
-                message: 'No token provided'
-            });
-        }
+       
 
         const token = authHeader.split(' ')[1];
 
